9845844ceb56c3666b21cdd9676257e7ade285c4
jawsfett <joseph-mark.triolo@uts.edu.au>
2023-03-01 12:00:48 +1100

    Suffering

--- Selenium_Firefox_Website_Test.py
+++ Selenium_Firefox_Website_Test.py
@@ -1,20 +1,27 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
-
-
+import os
+from selenium import webdriver
 
 
 
 s = Service('/usr/local/bin/geckodriver')
 options = webdriver.FirefoxOptions()
 options = Options()
-#options.add_argument("--ignore-certificate-errors")
 options.add_argument("--private")
-#options.add_argument("--headless")
 options.add_argument("--window-size=1920,1080")
 
 
 driver = webdriver.Firefox(service=s, options=options)
-# navigate to eoi webpage
+
 driver.get('https://www.uts.edu.au/')
+
+
+try:
+    next_button = driver.find_element(By.ID, "node/4087").click()
+
+finally:
+    driver.quit()
