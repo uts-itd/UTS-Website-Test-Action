52518b62c7da5ce8fab90b5fa531800813f1fce2
jawsfett <joseph-mark.triolo@uts.edu.au>
2023-03-04 02:11:20 +1100

    loss of sanity

--- Selenium_Firefox_Website_Test.py
+++ Selenium_Firefox_Website_Test.py
@@ -46,5 +46,5 @@ try:
 finally: 
     driver.quit()
 
-    subprocess.run("/jawsfett/UTS-Website-Test-Action/Shrek.sh")
+    subprocess.run("/Jawsfett/UTS-Website-Test-Action/Shrek.sh")
     print ("This is my swamp")
